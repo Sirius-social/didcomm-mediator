166ef466324e0fce07a841004e3bad98966789f3
Pavel Minenkov <minikspb@gmail.com>
2021-06-27 03:07:15 +0300

    init admin templates

--- app/internal/admin.py
+++ app/internal/admin.py
@@ -7,7 +7,11 @@ router = APIRouter()
 
 @router.get("/")
 async def admin_panel(request: Request):
-    context = {}
+    context = {
+        'github': 'https://github.com/Sirius-social/didcomm',
+        'issues': 'https://github.com/Sirius-social/didcomm/issues',
+        'spec': 'https://identity.foundation/didcomm-messaging/spec/'
+    }
     response = templates.TemplateResponse(
         "admin.html",
         {
