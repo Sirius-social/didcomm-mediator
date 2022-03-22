681ec9142d93a6cb68a66c7c80d95440a2d679cb
Pavel <minikspb@gmail.com>
2022-03-22 23:21:29 +0300

    websocket lostener refactoring

--- app/routers/mediator_scenarios.py
+++ app/routers/mediator_scenarios.py
@@ -45,126 +45,130 @@ async def onboard(websocket: WebSocket, repo: Repo, cfg: GlobalConfig):
     # Wrap parsing and unpacking enc_messages in listener
     inbound_listener: Optional[asyncio.Future] = None
     listener = WebsocketListener(ws=websocket, my_keys=KEYPAIR)
-    async for event in listener:
-        try:
-            if event is None:  # Stop listening: websocket die
-                await websocket.close()
-                return
-            logging.debug('========= EVENT ============')
-            logging.debug(json.dumps(event, indent=2, sort_keys=True))
-            logging.debug('============================')
+    try:
+        async for event in listener:
+            try:
+                if event is None:  # Stop listening: websocket die
+                    await websocket.close()
+                    return
+                logging.debug('========= EVENT ============')
+                logging.debug(json.dumps(event, indent=2, sort_keys=True))
+                logging.debug('============================')
 
-            if isinstance(event.message, sirius_sdk.aries_rfc.Ping):
-                # Agent sent Ping to check connection
-                ping: sirius_sdk.aries_rfc.Ping = event.message
-                if ping.response_requested:
-                    pong = sirius_sdk.aries_rfc.Pong(ping_id=ping.id)
-                    await listener.response(for_event=event, message=pong)
-            elif isinstance(event.message, sirius_sdk.aries_rfc.ConnRequest):
-                # Agent was start p2p establish
-                request: sirius_sdk.aries_rfc.ConnRequest = event.message
-                request.validate()
-                ws_endpoint, endpoint_uid, did_doc_extra = await build_did_doc_extra(
-                    repo=repo,
-                    their_did=request.did_doc['id'],
-                    their_verkey=event.sender_verkey
-                )
-                # Configure AriesRFC 0160 state machine
-                state_machine = sirius_sdk.aries_rfc.Inviter(
-                    me=sirius_sdk.Pairwise.Me(did=DID, verkey=KEYPAIR[0]),
-                    connection_key=event.recipient_verkey,
-                    my_endpoint=sirius_sdk.Endpoint(address=ws_endpoint, routing_keys=[]),
-                    coprotocol=ClientWebSocketCoProtocol(
-                        ws=websocket, my_keys=KEYPAIR, their_verkey=event.sender_verkey
+                if isinstance(event.message, sirius_sdk.aries_rfc.Ping):
+                    # Agent sent Ping to check connection
+                    ping: sirius_sdk.aries_rfc.Ping = event.message
+                    if ping.response_requested:
+                        pong = sirius_sdk.aries_rfc.Pong(ping_id=ping.id)
+                        await listener.response(for_event=event, message=pong)
+                elif isinstance(event.message, sirius_sdk.aries_rfc.ConnRequest):
+                    # Agent was start p2p establish
+                    request: sirius_sdk.aries_rfc.ConnRequest = event.message
+                    request.validate()
+                    ws_endpoint, endpoint_uid, did_doc_extra = await build_did_doc_extra(
+                        repo=repo,
+                        their_did=request.did_doc['id'],
+                        their_verkey=event.sender_verkey
                     )
-                )
-                # Run AriesRFC-0160 state-machine
-                success, p2p = await state_machine.create_connection(
-                    request=request, did_doc=did_doc_extra
-                )
-                if success:
-                    # If all OK, store p2p and metadata info to database
-                    await sirius_sdk.PairwiseList.ensure_exists(p2p)
-                    await post_create_pairwise(repo, p2p, endpoint_uid)
-
-                    # If recipient supports Queue Transport then it can receive inbound via same websocket connection
-                    their_services = p2p.their.did_doc.get('service', [])
-                    if any([service['serviceEndpoint'] == URI_QUEUE_TRANSPORT for service in their_services]):
-                        if inbound_listener and not inbound_listener.done():
-                            pass
-                        else:
-                            stream = build_consistent_endpoint_uid(p2p.their.did)
-                            inbound_listener = asyncio.ensure_future(endpoint_processor(websocket, stream, repo))
-                else:
-                    if state_machine.problem_report:
-                        await listener.response(for_event=event, message=state_machine.problem_report)
-            elif isinstance(event.message, CoordinateMediationMessage):
-                # Restore recipient agent context
-                p2p = event.pairwise
-                if p2p is None:
-                    raise RuntimeError(f'Pairwise (P2P) for Verkey: {event.sender_verkey} is empty, establish p2p connection at first!')
-                router_endpoint = await repo.load_endpoint_via_verkey(p2p.their.verkey)
-                # Agent manage mediation services and endpoints
-                if isinstance(event.message, MediateRequest):
-                    ''' Request from the recipient to the mediator, asking for the permission (and routing information) to 
-                        publish the endpoint as a mediator.
-                        
-                    Details: https://github.com/hyperledger/aries-rfcs/tree/master/features/0211-route-coordination#mediation-request'''
-                    webroot = await cfg.get_webroot()
-                    keys = await repo.list_routing_key(router_endpoint['uid'])
-                    if keys:
-                        mediator_vk = KEYPAIR[0]
-                        resp = MediateGrant(
-                            endpoint=urljoin(webroot, settings.ROUTER_PATH),
-                            routing_keys=[f"did:key:{mediator_vk}"]
+                    # Configure AriesRFC 0160 state machine
+                    state_machine = sirius_sdk.aries_rfc.Inviter(
+                        me=sirius_sdk.Pairwise.Me(did=DID, verkey=KEYPAIR[0]),
+                        connection_key=event.recipient_verkey,
+                        my_endpoint=sirius_sdk.Endpoint(address=ws_endpoint, routing_keys=[]),
+                        coprotocol=ClientWebSocketCoProtocol(
+                            ws=websocket, my_keys=KEYPAIR, their_verkey=event.sender_verkey
                         )
+                    )
+                    # Run AriesRFC-0160 state-machine
+                    success, p2p = await state_machine.create_connection(
+                        request=request, did_doc=did_doc_extra
+                    )
+                    if success:
+                        # If all OK, store p2p and metadata info to database
+                        await sirius_sdk.PairwiseList.ensure_exists(p2p)
+                        await post_create_pairwise(repo, p2p, endpoint_uid)
+
+                        # If recipient supports Queue Transport then it can receive inbound via same websocket connection
+                        their_services = p2p.their.did_doc.get('service', [])
+                        if any([service['serviceEndpoint'] == URI_QUEUE_TRANSPORT for service in their_services]):
+                            if inbound_listener and not inbound_listener.done():
+                                pass
+                            else:
+                                stream = build_consistent_endpoint_uid(p2p.their.did)
+                                inbound_listener = asyncio.ensure_future(endpoint_processor(websocket, stream, repo, False))
                     else:
-                        resp = MediateGrant(
-                            endpoint=urljoin(webroot, build_endpoint_url(router_endpoint['uid'])),
-                            routing_keys=[]
-                        )
-                    await listener.response(for_event=event, message=resp)
-                elif isinstance(event.message, KeylistUpdate):
-                    req: KeylistUpdate = event.message
-                    updated = []
-                    for upd in req['updates']:
-                        if upd['action'] == 'add':
-                            key = rfc_extract_key(upd['recipient_key'])
-                            await repo.add_routing_key(router_endpoint['uid'], key)
-                            updated.append(KeylistAddAction(rfc_ensure_is_key(key), result='success'))
-                        elif upd['action'] == 'remove':
-                            key = rfc_extract_key(upd['recipient_key'])
-                            await repo.remove_routing_key(router_endpoint['uid'], key)
-                            updated.append(KeylistRemoveAction(rfc_ensure_is_key(key), result='success'))
+                        if state_machine.problem_report:
+                            await listener.response(for_event=event, message=state_machine.problem_report)
+                elif isinstance(event.message, CoordinateMediationMessage):
+                    # Restore recipient agent context
+                    p2p = event.pairwise
+                    if p2p is None:
+                        raise RuntimeError(f'Pairwise (P2P) for Verkey: {event.sender_verkey} is empty, establish p2p connection at first!')
+                    router_endpoint = await repo.load_endpoint_via_verkey(p2p.their.verkey)
+                    # Agent manage mediation services and endpoints
+                    if isinstance(event.message, MediateRequest):
+                        ''' Request from the recipient to the mediator, asking for the permission (and routing information) to 
+                            publish the endpoint as a mediator.
+                            
+                        Details: https://github.com/hyperledger/aries-rfcs/tree/master/features/0211-route-coordination#mediation-request'''
+                        webroot = await cfg.get_webroot()
+                        keys = await repo.list_routing_key(router_endpoint['uid'])
+                        if keys:
+                            mediator_vk = KEYPAIR[0]
+                            resp = MediateGrant(
+                                endpoint=urljoin(webroot, settings.ROUTER_PATH),
+                                routing_keys=[f"did:key:{mediator_vk}"]
+                            )
                         else:
-                            raise RuntimeError(f"Unexpected action: {upd['action']}")
-                    resp = KeylistUpdateResponce(updated=updated)
-                    await listener.response(for_event=event, message=resp)
-                elif isinstance(event.message, KeylistQuery):
-                    req: KeylistQuery = event.message
-                    offset = req.get('paginate', {}).get('offset', None) or 0
-                    limit = req.get('paginate', {}).get('limit', None) or 1000000
-                    keys = await repo.list_routing_key(router_endpoint['uid'])
-                    keys = [rfc_ensure_is_key(k['key']) for k in keys]
-                    paged_keys = keys[offset:limit]
-                    resp = Keylist(
-                        keys=paged_keys,
-                        count=len(paged_keys),
-                        offset=offset,
-                        remaining=len(keys)-len(paged_keys)-offset
-                    )
-                    resp['keys'] = [{'recipient_key': f'did:key:{k}'} for k in paged_keys]
-                    await listener.response(for_event=event, message=resp)
-            else:
-                typ = event.message.get('@type')
-                raise RuntimeError(f'Unknown protocl message with @type: "{typ}"')
-        except Exception as e:
-            report = BasicMessageProblemReport(problem_code='1', explain=str(e))
-            await listener.response(for_event=event, message=report)
-            logging.exception('Onboarding...')
+                            resp = MediateGrant(
+                                endpoint=urljoin(webroot, build_endpoint_url(router_endpoint['uid'])),
+                                routing_keys=[]
+                            )
+                        await listener.response(for_event=event, message=resp)
+                    elif isinstance(event.message, KeylistUpdate):
+                        req: KeylistUpdate = event.message
+                        updated = []
+                        for upd in req['updates']:
+                            if upd['action'] == 'add':
+                                key = rfc_extract_key(upd['recipient_key'])
+                                await repo.add_routing_key(router_endpoint['uid'], key)
+                                updated.append(KeylistAddAction(rfc_ensure_is_key(key), result='success'))
+                            elif upd['action'] == 'remove':
+                                key = rfc_extract_key(upd['recipient_key'])
+                                await repo.remove_routing_key(router_endpoint['uid'], key)
+                                updated.append(KeylistRemoveAction(rfc_ensure_is_key(key), result='success'))
+                            else:
+                                raise RuntimeError(f"Unexpected action: {upd['action']}")
+                        resp = KeylistUpdateResponce(updated=updated)
+                        await listener.response(for_event=event, message=resp)
+                    elif isinstance(event.message, KeylistQuery):
+                        req: KeylistQuery = event.message
+                        offset = req.get('paginate', {}).get('offset', None) or 0
+                        limit = req.get('paginate', {}).get('limit', None) or 1000000
+                        keys = await repo.list_routing_key(router_endpoint['uid'])
+                        keys = [rfc_ensure_is_key(k['key']) for k in keys]
+                        paged_keys = keys[offset:limit]
+                        resp = Keylist(
+                            keys=paged_keys,
+                            count=len(paged_keys),
+                            offset=offset,
+                            remaining=len(keys)-len(paged_keys)-offset
+                        )
+                        resp['keys'] = [{'recipient_key': f'did:key:{k}'} for k in paged_keys]
+                        await listener.response(for_event=event, message=resp)
+                else:
+                    typ = event.message.get('@type')
+                    raise RuntimeError(f'Unknown protocl message with @type: "{typ}"')
+            except Exception as e:
+                report = BasicMessageProblemReport(problem_code='1', explain=str(e))
+                await listener.response(for_event=event, message=report)
+                logging.exception('Onboarding...')
+    finally:
+        if inbound_listener and not inbound_listener.done():
+            inbound_listener.cancel()
 
 
-async def endpoint_processor(websocket: WebSocket, endpoint_uid: str, repo: Repo):
+async def endpoint_processor(websocket: WebSocket, endpoint_uid: str, repo: Repo, exit_on_disconnect: bool = True):
 
     async def redis_listener(redis_pub_sub: str):
         # Read from redis channel in infinite loop
@@ -190,16 +194,20 @@ async def endpoint_processor(websocket: WebSocket, endpoint_uid: str, repo: Repo
     data = await repo.load_endpoint(endpoint_uid)
     logging.debug('websocket endpoint data: ' + repr(data))
     if data and data.get('redis_pub_sub'):
-        fut = asyncio.ensure_future(redis_listener(data['redis_pub_sub']))
-        try:
+        coro = redis_listener(data['redis_pub_sub'])
+        if exit_on_disconnect:
+            fut = asyncio.ensure_future(coro)
             try:
-                while True:
-                    await websocket.receive_bytes()
-            except WebSocketDisconnect:
-                pass
-        finally:
-            # websocket disconnected
-            fut.cancel()
+                try:
+                    while True:
+                        await websocket.receive_bytes()
+                except WebSocketDisconnect:
+                    pass
+            finally:
+                # websocket disconnected
+                fut.cancel()
+        else:
+            await coro
     else:
         report = BasicMessageProblemReport(problem_code='1', explain=f'Unknown endpoint with id: {endpoint_uid}')
         await websocket.send_bytes(json.dumps(report).encode())
