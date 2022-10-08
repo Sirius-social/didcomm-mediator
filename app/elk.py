d27d58f7003bb75a904a647a760e6ddf94a01fe7
Pavel <minikspb@gmail.com>
2022-10-08 23:33:42 +0300

    refactoring logs for ELK

--- app/elk.py
+++ app/elk.py
@@ -8,6 +8,6 @@ class ElkJsonFormatter(jsonlogger.JsonFormatter):
 
     def add_fields(self, log_record, record, message_dict):
         super(ElkJsonFormatter, self).add_fields(log_record, record, message_dict)
-        log_record['@utc_timestamp'] = datetime.utcnow().isoformat()
+        log_record['@timestamp'] = datetime.utcnow().isoformat()
         log_record['level'] = record.levelname
         log_record['logger'] = record.name
