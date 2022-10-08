38246e7816b4245703636f2823caeeba457de733
Pavel <minikspb@gmail.com>
2022-10-08 23:10:59 +0300

    refactiring

--- app/core/utils.py
+++ app/core/utils.py
@@ -5,18 +5,16 @@ import sirius_sdk
 
 
 def info_p2p_event(p2p: Union[sirius_sdk.Pairwise, str], message: str, **context):
-    record = {
-        'message': message
-    }
+    extra = {}
     if isinstance(p2p, str):
-        record['did'] = p2p
+        extra['did'] = p2p
     elif isinstance(p2p, sirius_sdk.Pairwise):
-        record['did'] = p2p.their.did
-        record['verkey'] = p2p.their.verkey
-        record['label'] = p2p.their.label
-        record['endpoint'] = p2p.their.endpoint
+        extra['did'] = p2p.their.did
+        extra['verkey'] = p2p.their.verkey
+        extra['label'] = p2p.their.label
+        extra['endpoint'] = p2p.their.endpoint
     else:
         return
     if context:
-        record.update(context)
-    logging.info(record)
+        extra.update(context)
+    logging.info(message, extra=extra)
